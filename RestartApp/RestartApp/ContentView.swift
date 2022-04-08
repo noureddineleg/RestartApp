acea627670078a27188eb9fad51a288483d49a2e
Noureddine Legragui <noureddinelegragui@gmail.com>
2022-04-08 22:36:31 +0000

    [TASK-1] FEAT - Setup project

---
+++ RestartApp/RestartApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestartApp
+//
+//  Created by Noureddine Legragui on 8/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestartApp/RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Noureddine Legragui on 8/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
