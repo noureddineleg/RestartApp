5dc927067e59d04988ae23bc26c603b9da035b49
Noureddine Legragui <noureddinelegragui@gmail.com>
2022-04-09 20:13:44 +0000

    [TASK-6] FEAT - Setup onboarding header

--- RestartApp/RestartApp/Screens/OnboardingView.swift
+++ RestartApp/RestartApp/Screens/OnboardingView.swift
@@ -16,17 +16,42 @@ struct OnboardingView: View {
     // MARK: - View
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        } //: VSTACK
+            VStack(spacing: 20) {
+                
+                // MARK: - Header
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much we put in giving
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                } //: HEADER
+                
+                // MARK: - Center
+                
+                // MARK: - Footer
+                
+            } //: VSTACK
+        } //: ZSTACK
     }
+    
 }
 
 // MARK: - Preview
